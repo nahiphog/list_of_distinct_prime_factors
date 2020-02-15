eb76c23763f162f7015991fdd9133e550894d5cb
nahiphog <51304920+nahiphog@users.noreply.github.com>
2020-02-15 19:52:23 +0800

    Update miscellaneous.py

--- miscellaneous.py
+++ miscellaneous.py
@@ -5,3 +5,11 @@ input_number = 10 ** 2
 quadratic_residues = [ (integer ** 2) % input_number for integer in range(0, input_number )]
 quadratic_residues.sort()
 print(set(quadratic_residues))
+
+######################################################
+##### [2] Last few digits
+######################################################
+for integer in range(1000, 9999 + 1):
+    if len(str(integer ** 2)) == 8:
+        if str(integer ** 2 - integer).endswith('0000'):
+            print(integer)
