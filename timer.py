1cc6df5f103e2f47e82e2260c9971000e2166cbc
nahiphog <51304920+nahiphog@users.noreply.github.com>
2020-03-01 02:47:14 +0800

    Update timer.py

--- timer.py
+++ timer.py
@@ -7,9 +7,18 @@ start_time = datetime.now()
 print(f"The total runtime of this script is {datetime.now() - start_time}.\n")
 
 ######################################################
-###### [2] In seconds only
+###### [2A] In seconds only
 ######################################################
 import time
 start_time = time.time()
 
 print("This script took %s seconds to run." % (time.time() - start_time))
+
+######################################################
+###### [2B] In proper time unit, especially when it can run for over a minute
+######################################################
+from humanfriendly import format_timespan
+from time import time
+start_time = time()
+
+print(f"This script took { format_timespan( time() - start_time) } to run." )
