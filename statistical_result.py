46bcfebf9c82758b592f0bff201890681c5d0f8b
nahiphog <51304920+nahiphog@users.noreply.github.com>
2020-02-29 17:52:44 +0800

    Update statistical_result.py

--- statistical_result.py
+++ statistical_result.py
@@ -30,8 +30,8 @@ final_report = [
 
 # Print the table of the statistical result
 headers = ["Statistical measure", "Output"]
-print(f"\n\t\tMonte Carlo simulation for {total_number_of_trials} trials.\n" + 
-tabulate(final_report,headers, tablefmt="psql" , colalign=("right",))
+print(f"\n\t\tMonte Carlo simulation for {total_number_of_trials:,} trials.\n" + 
+tabulate(final_report,headers, tablefmt="psql" , colalign=("right",)))
 
 ############################################################
 ######### Accompany this table with a histogram ############
