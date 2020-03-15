882b9c85709ab41f7cf39cb3c2e3d61f82bc82b3
nahiphog <51304920+nahiphog@users.noreply.github.com>
2020-03-16 00:06:44 +0800

    Update recreational_number_theory.py

--- recreational_number_theory.py
+++ recreational_number_theory.py
@@ -47,3 +47,57 @@ print ( def is_pandigital(num,starting_digit,stopping_digit) )
 from eulerlib.etc import *
 print ( word_numerical_val('randomword') )
 
+######################################################
+##### [7] Pell's equation solution
+######################################################
+# Generate the first first solutions for Pell's equation x^2 - N * y^2 = 1 for a given positive integer N
+from math import sqrt, floor
+from tabulate import tabulate
+
+# INPUT HERE: From x^2 - N * y^2 = 1
+n = 29
+# INPUT HERE: Print how many solutions? 
+desired_solutions = 10
+
+integer_part_of_sqrt_n = int( sqrt(n) )
+generate_continued_fraction_list = []
+modify_this_number = sqrt(n) - integer_part_of_sqrt_n
+for index in range(30):
+    use_this_number  = modify_this_number ** -1
+    generate_continued_fraction_list.append( floor(use_this_number ) )
+    modify_this_number = use_this_number - floor(use_this_number)
+
+how_long_is_this_list = len(generate_continued_fraction_list)
+
+numerator_list = [ 1 , generate_continued_fraction_list[1] ]
+denominator_list = [ generate_continued_fraction_list[0] , generate_continued_fraction_list[0] * generate_continued_fraction_list[1] + 1 ]
+generate_continued_fraction_list.pop(0)
+generate_continued_fraction_list.pop(0)
+while len(generate_continued_fraction_list) > 0:
+    use_this_number = generate_continued_fraction_list.pop(0)
+    numerator_list.append( use_this_number * numerator_list[-1] + numerator_list[-2] )
+    denominator_list.append( use_this_number * denominator_list[-1] + denominator_list[-2] )
+
+x_solution, y_solution = [] , []
+# Find the fundamental solution
+for element in range(how_long_is_this_list): 
+    x = numerator_list[element] + integer_part_of_sqrt_n * denominator_list[element]
+    y = denominator_list[element]
+    if (x ** 2) - n * (y ** 2) == 1:
+        x_solution.append(x)
+        y_solution.append(y)
+        break
+
+# Use recurrence relation to generate more Pell's solution
+for index in range(desired_solutions):
+    x_last_solution = x_solution[-1]
+    y_last_solution = y_solution[-1]
+
+    x_solution.append( x_solution[0] * x_last_solution + n * y_solution[0] * y_last_solution )
+    y_solution.append( x_solution[0] * y_last_solution + y_solution[0] * x_last_solution )
+
+print_this_table = [ [ x_solution[element] , y_solution[element] ] for element in range(desired_solutions) ]
+
+# Output solution
+print(f"\nThe first {desired_solutions} solutions to the Pell's equation [ x^2 - {n} y^2 = 1 ] are:\n\n" +  
+    tabulate(print_this_table, ["x", "y"], tablefmt="fancy_grid" , colalign=("right",)) )
