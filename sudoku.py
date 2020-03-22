d71db70c893a0ccf6b7ccd5ca82b921910304b6a
nahiphog <51304920+nahiphog@users.noreply.github.com>
2020-03-22 12:52:22 +0800

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,5 @@
 ######################################################
-###### [1] Solve a Classic Sudoku puzzle with a unique solution
+###### [1] Solve a Classic Sudoku puzzle and display all solutions
 ######################################################
 
 import numpy as np
@@ -41,8 +41,10 @@ def possible_movement(x,y,n):
             
     return True
 
+total_number_solutions = 0
+
 def solve_this_sudoku():
-    global sudoku_grid
+    global sudoku_grid, total_number_solutions
     for y in range(0, 8 + 1):
         for x in range(0, 8 + 1):
             if sudoku_grid[y][x] == 0:
@@ -51,7 +53,55 @@ def solve_this_sudoku():
                         sudoku_grid[y][x] = n
                         solve_this_sudoku()
                         sudoku_grid[y][x] = 0
-                return          
+                return
+
+    total_number_solutions += 1
+    # all_possible_grids.append(sudoku_grid)
+    print(total_number_solutions)
     print(np.matrix(sudoku_grid))
+    print("\n")
 
 solve_this_sudoku()
+
+######################################################
+###### [2] Solve a X-Sudoku puzzle and display all solutions
+######################################################
+
+x_sudoku = True
+
+# Backtracking function
+def possible_movement(x,y,n):
+    global sudoku_grid
+
+    # Sudoku rule 1: Check whether whether N has appeared in that row
+    for i in range(9):
+        if sudoku_grid[y][i] == n:
+            return False
+    
+    # Sudoku rule 2: Check whether whether N has appeared in that column
+    for i in range(9):
+        if sudoku_grid[i][x] == n:
+            return False
+    
+    # Sudoku rule 3: Check whether N has appeared in that 3x3 block
+    x0 = (x//3) * 3
+    y0 = (y//3) * 3
+    for i in range(0,3):
+        for j in range(0,3):
+            if sudoku_grid[y0 + i][x0 + j] == n:
+                return False
+
+    # Custom sudoku rule (1): X-sudoku: Check whether the cell is in one of the diagonals. If yes, check whether N has has appeared in its respective diagonal(s)
+    if x_sudoku == True:
+        # Check whether its in the main diagonal
+        if (x == y):
+            diagonal_list = [ sudoku_grid[position][position] for position in range(0, 8 + 1) ]
+            if n in diagonal_list:
+                return False
+        # Check whether its in the main diagonal
+        if (x + y == 8):
+            anti_diagonal_list = [ sudoku_grid[position][8 - position] for position in range(0,8 + 1) ]
+            if n in anti_diagonal_list:
+                return False
+            
+    return True
