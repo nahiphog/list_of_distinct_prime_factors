29d18b92eb329fe2db60aaa9d203da4bb5e741ac
Goh Pi Han <51304920+nahiphog@users.noreply.github.com>
2020-05-20 03:55:20 +0800

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -1,25 +1,55 @@
-##############################
-############ LINE GRAPH
-##############################
 import matplotlib.pyplot as plt 
   
-# x axis values 
-x = [*range(1,21)]
-# corresponding y axis values 
-y = [frequencies_of_each_roll[index][1] /(20 ** 4) * 100 for index in range(20)] 
-z = [frequencies_of_each_roll[index][2] /(20 ** 4) * 100 for index in range(20)] 
+################ Some fake data
+
+die = range(1, 21)
+AoDRolls = [max(min(roll[0], roll[1]), min(roll[2], roll[3])) for roll in it.product(die, repeat=4)]
+DoARolls = [min(max(roll[0], roll[1]), max(roll[2], roll[3])) for roll in it.product(die, repeat=4)]
+singleRolls = die
+
+AoDtoPlot ,DoAtoPlot, singletoPlot = [], [], []
+
+for N in die:
+  AoDWins = len([i for i in AoDRolls if i >= N])
+  DoAWins = len([i for i in DoARolls if i >= N])
+  singleWins = len([i for i in singleRolls if i >= N])
+  AoDtoPlot.append(100 * AoDWins / len(AoDRolls))
+  DoAtoPlot.append(100 * DoAWins / len(DoARolls))
+  singletoPlot.append(100 * singleWins / len(singleRolls))
   
-# Plot the graph and its legend
-plt.plot(x,y, x,z)
-plt.legend([ 'ADV of DISADV', 'DISADV of ADV'])
-  
-# naming the x axis 
-plt.xlabel('Roll number')  
-# naming the y axis 
-plt.ylabel('Relative frequency (%)') 
-  
+################
+
 # Give a title to my graph 
 plt.title('Probability distribution for different strategies') 
   
-# function to show the plot 
-plt.show() 
+# Show dimension = Length x Height
+figure = plt.figure(figsize=(10,6))
+
+# Plot range of graph: Range = [xmin, xmax, ymin, ymax]
+plt.axis([0, 20, 0, 110]) 
+
+# Plot all the relevant graphs with colors, labels, and linewidth
+plt.plot(die, AoDtoPlot, color = 'r', label = 'AoD', linewidth = 3)
+plt.plot(die, DoAtoPlot, color = 'g', label = 'DoA', linewidth = 3)
+plt.plot(die, singletoPlot, color = 'b', label = 'singles', linewidth = 3)
+
+# Any grid?
+plt.grid(axis="x") # Options: x, y, both
+
+# Label which x-axis coordinates?
+plt.xticks(die)
+
+# Label which y-axis coordinates?
+plt.yticks([0, 10, 33, 100])
+
+# Label the X-axis and Y-axis
+plt.xlabel("$N$")
+plt.ylabel("Probability of rolling $\geqslant N$ (in %)")
+
+# Show legend
+plt.legend()
+
+# Save figure in your local directory
+plt.savefig('graph.png')
+
+# Show graph
